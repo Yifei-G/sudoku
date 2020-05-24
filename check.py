dad0656143c762a34ef5fcf56473be9ca135eeb9
Yifei Ge <yifei.ge91@gmail.com>
2020-05-24 23:01:38 +0200

    Change function name checkBoard to isBoardComplete to understand better the function's goal

--- check.py
+++ check.py
@@ -4,7 +4,7 @@ taileLevel = [[0, 1, 2], [3, 4, 5], [6, 7, 8]]
 
 
 # A function to check if the board is full
-def checkBoard(board):
+def isBoardComplete(board):
     for row in range(0, 9):
         for column in range(0, 9):
             if board[row][column] == "_":
